e53eb19a00abfd56d5a9317b5e38b89b35e35c16
joaoalrodrigues <48808876+joaoalrodrigues@users.noreply.github.com>
2021-02-08 18:17:15 +0100

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,41 @@
 import styled from 'styled-components'
+import Widget from '../src/components/Widget'
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import { bg } from "../db.json";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const QuizContainer = styled.div`
+    width: 100%;
+    max-width: 350px;
+    padding-top: 45px;
+    margin: auto 10%;
+    @media screen and (max-width: 500px){
+        margin:auto;
+        padding: 15px;
+    }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+    return (
+        <QuizBackground backgroundImage={bg}>
+            <QuizContainer>
+                <Widget>
+                    <Widget.Header>
+                        <h1>A Roda do Tempo</h1>
+                    </Widget.Header>
+                    <Widget.Content>
+                        <p>lorem ipsum dolor sit amet...</p>
+                    </Widget.Content>
+                </Widget>
+                <Widget>
+                    <Widget.Content>
+                        <p>lorem ipsum dolor sit amet...</p>
+                    </Widget.Content>
+                </Widget>
+                <Footer />
+            </QuizContainer>
+            <GitHubCorner projectUrl="https://github.com/joaoalrodrigues" />
+        </QuizBackground>
+    );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+const FooterWrapper = styled.footer`
+    align-items: center;
+    background-color:  ${({ theme }) => theme.colors.secondary};
+    border-radius: 4px;
+    display: flex;
+    justify-content: flex-start;
+    padding: 20px;
+
+    img {
+        width: 58px;
+        margin-right: 23px;
+    }
+
+    a {
+        color: white;
+        text-decoration: none;
+        transition: .3s;
+        &:hover,
+        &:focus {
+            opacity: .5;
+        }
+    }
+
+    span {
+        text-decoration: underline;
+    }
+`;
+
+export default function Footer(props) {
+    return (
+        <FooterWrapper>
+            <a href="https://www.alura.com.br/">
+                <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+            </a>
+            <p>
+                Criado durante
+        {' '}
+        a
+        {' '}
+                <a href="https://www.alura.com.br/">
+                    <span>Imersão React da Alura</span>
+                </a>
+            </p>
+        </FooterWrapper>
+    );
+};
